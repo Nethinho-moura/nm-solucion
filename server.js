4348c0da029b7cc0ae2fb3c5f4366ddf5a948fa8
Nethinho-moura <nethinho16@gmail.com>
2026-05-15 09:05:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-[]
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+``
